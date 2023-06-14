b82afa7e395edc5dc833bd6787f3a30838c42cab
David <davidteng95@gmail.com>
2023-06-14 05:38:27 +0000

    add POST route for /login

--- express_server.js
+++ express_server.js
@@ -87,6 +87,13 @@ app.get("/hello", (req, res) => {
   res.redirect('/urls');
 });
 
+ app.post('/login', (req, res) => {
+  const { username } = req.body;
+
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
